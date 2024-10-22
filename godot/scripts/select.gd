e7a628236e941b4c276f7377a4a083efb2c2a038
wends05 <cxnner05@gmail.com>
2024-10-22 18:01:07 +0800

    sample transition

--- godot/scripts/select.gd
+++ godot/scripts/select.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+
+@onready var rightDoor = $RightDoor
+@onready var leftDoor = $LeftDoorCutscene
+func _ready() -> void:
+	var tween = create_tween()
+	tween.tween_property(rightDoor, "position", Vector2(576, rightDoor.position.y), 0.5).set_ease(Tween.EASE_IN_OUT)
+	tween.set_parallel()
+	tween.tween_property(leftDoor, "position", Vector2(576, leftDoor.position.y), 0.5).set_ease(Tween.EASE_IN_OUT)
